edf3698e067ea94f74e95180ec9beff4ad6f11b5
Jonas Juselius <jonas.juselius@itpartner.no>
2019-05-06 10:34:01 +0200

    Reformat Client.

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -2,16 +2,11 @@ module Client
 
 open Elmish
 open Elmish.React
-
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 open Fable.PowerPack.Fetch
-
 open Thoth.Json
-
 open Shared
-
-
 open Fulma
 
 
@@ -28,8 +23,6 @@ type Msg =
 | Decrement
 | InitialCountLoaded of Result<Counter, exn>
 
-
-
 let initialCounter = fetchAs<Counter> "/api/init" (Decode.Auto.generateDecoder())
 
 // defines the initial state and initial command (= side-effect) of the application
@@ -42,7 +35,6 @@ let init () : Model * Cmd<Msg> =
             (Ok >> InitialCountLoaded)
             (Error >> InitialCountLoaded)
     initialModel, loadCountCmd
-
 
 
 // The update function computes the next state of the application based on the current state and the incoming events/messages
@@ -65,50 +57,61 @@ let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
 
 let safeComponents =
     let components =
-        span [ ]
-           [
-             a [ Href "https://github.com/giraffe-fsharp/Giraffe" ] [ str "Giraffe" ]
-             str ", "
-             a [ Href "http://fable.io" ] [ str "Fable" ]
-             str ", "
-             a [ Href "https://elmish.github.io/elmish/" ] [ str "Elmish" ]
-             str ", "
-             a [ Href "https://fulma.github.io/Fulma" ] [ str "Fulma" ]
-           ]
+        span [] [
+            a [ Href "https://github.com/giraffe-fsharp/Giraffe" ] [ str "Giraffe" ]
+            str ", "
+            a [ Href "http://fable.io" ] [ str "Fable" ]
+            str ", "
+            a [ Href "https://elmish.github.io/elmish/" ] [ str "Elmish" ]
+            str ", "
+            a [ Href "https://fulma.github.io/Fulma" ] [ str "Fulma" ]
+        ]
 
-    span [ ]
-        [ strong [] [ str "SAFE Template" ]
-          str " powered by: "
-          components ]
+    span [] [
+        strong [] [ str "SAFE Template" ]
+        str " powered by: "
+        components
+    ]
 
 let show = function
 | { Counter = Some counter } -> string counter.Value
 | { Counter = None   } -> "Loading..."
 
 let button txt onClick =
-    Button.button
-        [ Button.IsFullWidth
-          Button.Color IsPrimary
-          Button.OnClick onClick ]
-        [ str txt ]
+    Button.button [
+        Button.IsFullWidth
+        Button.Color IsPrimary
+        Button.OnClick onClick
+    ] [ str txt ]
 
 let view (model : Model) (dispatch : Msg -> unit) =
-    div []
-        [ Navbar.navbar [ Navbar.Color IsPrimary ]
-            [ Navbar.Item.div [ ]
-                [ Heading.h2 [ ]
-                    [ str "SAFE Template" ] ] ]
-
-          Container.container []
-              [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
-                    [ Heading.h3 [] [ str ("Press buttons to manipulate counter: " + show model) ] ]
-                Columns.columns []
-                    [ Column.column [] [ button "-" (fun _ -> dispatch Decrement) ]
-                      Column.column [] [ button "+" (fun _ -> dispatch Increment) ] ] ]
-
-          Footer.footer [ ]
-                [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
-                    [ safeComponents ] ] ]
+    div [] [
+        Navbar.navbar [ Navbar.Color IsPrimary ] [
+            Navbar.Item.div [] [
+                Heading.h2 [] [ str "SAFE Template" ]
+            ]
+        ]
+        Container.container [] [
+            Content.content [
+                Content.Modifiers [
+                    Modifier.TextAlignment (Screen.All, TextAlignment.Centered)
+                ]
+            ] [
+                Heading.h3 [] [ str ("Press buttons to manipulate counter: " + show model) ]
+            ]
+            Columns.columns [] [
+                Column.column [] [ button "-" (fun _ -> dispatch Decrement) ]
+                Column.column [] [ button "+" (fun _ -> dispatch Increment) ]
+            ]
+        ]
+        Footer.footer [] [
+            Content.content [
+                Content.Modifiers [
+                    Modifier.TextAlignment (Screen.All, TextAlignment.Centered)
+                ]
+            ] [ safeComponents ]
+        ]
+    ]
 
 #if DEBUG
 open Elmish.Debug
